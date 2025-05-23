6393cb21ceec8dd728c61bd9de9db8ab61b8ea12
matheushodecker <matheus.hodecker.santos@gmail.com>
2025-05-23 14:20:48 -0300

    feat: inclusão da app de upload e associação de imagens

--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from click.utils import R
@@ -10,6 +12,8 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 
 from core.views import CategoriaViewSet, UserViewSet, EditoraViewSet, AutorViewSet, LivroViewSet
+
+from uploader.router import router as uploader_router
 
 router = DefaultRouter()
 
@@ -33,6 +37,10 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name='schema'),
         name='redoc',
     ),
+    #Uploader
+    path("api/media/", include(uploader_router.urls)),
     # API
     path('api/', include(router.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -1,7 +1,8 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
 
 from core.models import Livro
-
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
 
 
 
@@ -12,12 +13,23 @@ class LivroListSerializer(ModelSerializer):
         
 
 class LivroRetrieveSerializer(ModelSerializer):
+    capa = ImageSerializer(required=False)
+
     class Meta:
         model = Livro
-        fields = "__all__"
+        fields = '__all__'
         depth = 1
 
 class LivroSerializer(ModelSerializer):
+    capa_attachment_key = SlugRelatedField(
+        source='capa',
+        queryset=Image.objects.all(),
+        slug_field='attachment_key',
+        required=False,
+        write_only=True,
+    )
+    capa = ImageSerializer(required=False, read_only=True)
+
     class Meta:
         model = Livro
-        fields = "__all__"
+        fields = '__all__'
