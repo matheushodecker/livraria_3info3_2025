ce3f5d9d3a23f6b2b83f894fd14e149f534c2150
matheushodecker <matheus.hodecker.santos@gmail.com>
2025-06-10 08:49:22 -0300

    permissoes ma viewset

--- core/views/categoria.py
+++ core/views/categoria.py
@@ -8,3 +8,5 @@ from core.serializers.categoria import CategoriaSerializer
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer  
+
+    # permission_classes = [IsAuthenticated]
